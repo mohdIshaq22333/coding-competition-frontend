4e4dfa0f4e54070f6e53a7bc85ed92fbe3c4298d
mohdIshaq22333 <ishaqbux24000@gmail.com>
2023-07-21 10:31:33 +0530

    first commit

---
+++ components/editor.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import Editor from "@monaco-editor/react";
+import { Button, Flex, Text } from "@chakra-ui/react";
+import { CheckIcon, CloseIcon } from "@chakra-ui/icons";
+
+function Editior({ handleSubmit, status = {} }) {
+  const [code, setCode] = React.useState(`
+  // Don't edit the function
+  function main(parameter){
+    // write your code here
+     console.log(parameter);
+  }
+  `);
+  return (
+    <>
+      {" "}
+      <Editor
+        height="50vh"
+        width="90vw"
+        defaultLanguage="javascript"
+        onChange={setCode}
+        value={code}
+      />
+      <Flex
+        direction="row"
+        alignItems="center"
+        gap={10}
+        justifyContent="Center"
+        marginTop={10}
+        marginBottom={5}
+      >
+        {status?.casesResult?.map((val) => (
+          <Flex
+            direction="row"
+            alignItems="center"
+            justifyContent="center"
+            key={val?.case}
+          >
+            {val?.passed ? (
+              <CheckIcon color="green" w={4} mr={2} h={4} />
+            ) : (
+              <CloseIcon color="red" w={3} mr={2} h={3} />
+            )}
+            <Text fontSize={["md", "lg", "xl"]}>{val?.case}</Text>
+          </Flex>
+        ))}
+      </Flex>
+      {status?.err && <Text>{status?.err}</Text>}
+      <Button
+        onClick={() => handleSubmit(code)}
+        isLoading={status?.loading}
+        loadingText="Loading"
+        colorScheme="teal"
+        size="md"
+      >
+        Submit
+      </Button>
+    </>
+  );
+}
+
+export default Editior;
---
+++ src/App.jsx
@@ -0,0 +1,109 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Editor from "../components/editor";
+import { Text, Box, Flex, useMediaQuery } from "@chakra-ui/react";
+
+import "./App.css";
+
+function App() {
+  const [isSmallerThan768] = useMediaQuery("(max-width: 767px)");
+  const [status, setStatus] = useState({});
+  const [problem, setProblem] = useState(null);
+
+  const handleSubmit = (code) => {
+    if (!code) return;
+    setStatus({
+      loading: true,
+    });
+    axios
+      .post(`http://localhost:3000/api/problems/${problem?.id}`, { code })
+      .then((val) => {
+        setStatus({
+          loading: false,
+          casesResult: val?.data?.casesResult || [],
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        setStatus({
+          loading: false,
+          err:
+            err?.response?.data?.error ||
+            err?.message ||
+            "Something went wrong!",
+        });
+      });
+  };
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/api/problems/${Math.ceil(Math.random() * 3)}`)
+      .then((val) => {
+        setProblem(val?.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        setStatus({
+          questionFetchingError:
+            err?.response?.data?.error ||
+            err?.message ||
+            "Something went wrong!",
+        });
+      });
+  }, []);
+
+  return (
+    <>
+      <Box p={4}>
+        <div className="container">
+          {status?.questionFetchingError && (
+            <Text fontSize={["xl", "2xl", "3xl"]} marginBottom={8}>
+              {status?.questionFetchingError}
+            </Text>
+          )}
+          <Text fontSize={["xl", "2xl", "3xl"]} marginBottom={8}>
+            {problem?.title}
+          </Text>
+          <Text fontSize={["md", "lg", "xl"]} marginBottom={10}>
+            {problem?.question}
+          </Text>
+          {problem?.examples && (
+            <Flex
+              direction={isSmallerThan768 ? "column" : "row"}
+              alignItems={isSmallerThan768 ? "flex-start" : "center"}
+              gap={10}
+              marginTop={5}
+              marginBottom={10}
+            >
+              {problem?.examples?.map((val, index) => {
+                return (
+                  <Flex
+                    direction="row"
+                    alignItems="baseline"
+                    gap={5}
+                    key={index}
+                  >
+                    <Text fontSize={["sm", "md", "lg"]}>Example: {` `}</Text>
+                    <Box>
+                      <Text textAlign={"start"} fontSize={["sm", "md", "lg"]}>
+                        {val?.input}
+                      </Text>
+                      <Text textAlign={"start"} fontSize={["sm", "md", "lg"]}>
+                        {val?.output}
+                      </Text>
+                    </Box>
+                  </Flex>
+                );
+              })}
+            </Flex>
+          )}
+          {problem?.title && (
+            <Editor handleSubmit={handleSubmit} status={status} />
+          )}
+        </div>
+      </Box>
+    </>
+  );
+}
+
+export default App;
